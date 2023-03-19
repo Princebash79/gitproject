c23a8c8f77d163ea9b9407f99a2bcd30861ba53e
Princebash79 <chiefprince79@gmail.com>
2023-03-19 21:25:13 +0000

    adds me too

---
+++ Prince.sh
@@ -0,0 +1,38 @@
+Josh Freeman 9:13 PM
+you do again only when you are using new terminal with different Local Ip
+Michael Acheampong  to  Everyone 9:17 PM
+if you have the scripts saved, just copy and paste in vi
+JOMACS - Johnson  to  Everyone 9:18 PM
+b2.sh bashass.sh bash.sh black.mp3 bubu.avi dame.mov david.flac dev.log
+dino.log edochie.jpg efe.log games.avi hunger.mov mapple.jnp naija.jpg
+nana.mov peace.py pete.png python.py shata.flac sheriff.flac toyin.pdf
+van.avi wale.mp3 wike.avi wiz.mp3 yul.jpg yvonne.txt zanku.mp3 ghana.avi
+Richmond  to  Everyone 9:19 PM
+Can’t see it in the chats
+Richard K. Osei  to  Everyone 9:19 PM
+you pasted only the files
+Richmond  to  Everyone 9:19 PM
+The last thing Johnson pasted are the files
+JOMACS - Johnson  to  Everyone 9:21 PM
+# !/bin/bash 
+# Author: Johnson
+
+touch b2.sh bashass.sh bash.sh black.mp3 bubu.avi dame.mov david.flac dev.log
+dino.log edochie.jpg efe.log games.avi hunger.mov mapple.jnp naija.jpg
+nana.mov peace.py pete.png python.py shata.flac sheriff.flac toyin.pdf
+van.avi wale.mp3 wike.avi wiz.mp3 yul.jpg yvonne.txt zanku.mp3 ghana.avi
+
+mkdir music images videos
+
+mv *.mp3 *.flac music
+
+mv *.jpg *.png images 
+
+mv *.avi *.mov videos 
+
+rm *.log
+
+tree
+Richmond  to  Everyone 9:21 PM
+thanks
+JOMACS - Johnson 9:21 PM
